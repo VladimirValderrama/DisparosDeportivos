5f30abcdd2413a3d02be701827fa39b88ef5654b
VladimirValderrama <vavalderrama@uc.cl>
2026-01-03 20:45:37 -0300

    Noticias

--- public/noticias/noticias.js
+++ public/noticias/noticias.js
@@ -1,27 +1,40 @@
 fetch('articulos.json')
   .then(response => response.json())
   .then(noticias => {
-    const container = document.getElementById('noticias-container')
+    const container = document.getElementById('noticias-container');
 
     noticias.forEach(noticia => {
-      container.innerHTML += `
+      container.insertAdjacentHTML('beforeend', `
         <div class="col-md-4 mb-4">
           <div class="card h-100 shadow-sm">
-            <img src="../${noticia.imagen}" class="card-img-top" loading="lazy">
+            
+            <a href="articulo.html?id=${noticia.id}">
+              <img src="../${noticia.imagen}" 
+                   class="card-img-top" 
+                   loading="lazy"
+                   alt="${noticia.titulo}">
+            </a>
+
             <div class="card-body">
-              <h5 class="card-title">${noticia.titulo}</h5>
+              <h5 class="card-title">
+                <a href="articulo.html?id=${noticia.id}" 
+                   class="text-decoration-none text-dark">
+                  ${noticia.titulo}
+                </a>
+              </h5>
+
               <p class="card-text">
                 <strong>${noticia.fecha}</strong> - ${noticia.lugar}
               </p>
-              <a href="articulo.html?id=${noticia.id}" class="btn btn-primary btn-sm">
+
+              <a href="articulo.html?id=${noticia.id}" 
+                 class="btn btn-primary btn-sm">
                 Ver artículo
               </a>
             </div>
           </div>
         </div>
-      `
-    })
+      `);
+    });
   })
-  .catch(error => {
-    console.error('Error cargando noticias:', error)
-  })
+  .catch(error => console.error('Error cargando noticias:', error));

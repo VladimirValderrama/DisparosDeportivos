0e1804a01da01dae4c17520b2e984f066b7c3256
VladimirValderrama <vavalderrama@uc.cl>
2026-03-03 19:20:09 -0300

    dfgd

--- server.js
+++ server.js
@@ -150,3 +150,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
+
+
+app.use(express.static("public", {
+  extensions: ["html"]
+}));
